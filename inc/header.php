d70a19eac5a8604c848dbf2809f2e1eb654d1308
Gaurav Jagiasi <gaurav@jagiasi.com>
2018-04-23 02:42:45 +0530

    Initial commit

---
+++ inc/header.php
@@ -0,0 +1,6 @@
+<script>
+
+</script>
+
+<link href="/css/custom.css" rel="stylesheet" type="text/css" /
+<link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet" />
---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+  <!-- Include Page details -->
+  <meta charset="UTF-8">
+  <title> Frigg Foods </title>
+
+  <!-- Include Header -->
+  <?php
+  ini_set("include_path", "inc");
+  include 'header.php';
+  ?>
+
+</head>
+
+<body>
+<div class="home-bg">
+  <video id="home-bgvid" autoplay loop>
+    <source src="/video/main-bg.mp4" type="video/mp4" />
+    <source src="/video/mountains-bg.webm" type="video/webm" />
+  </video>
+</div>
+
+<div class="home-bg-text">
+  <img src="/img/frigg-main-600.png" class="main-logo responsive"/>
+  <h1> Interesting times are on the horizon. </h1>
+  <h2> <a href="">Contact</a>  | <a href="">Learn More</a></h2>
+
+</div>
+
+
+</body>
+</html>
